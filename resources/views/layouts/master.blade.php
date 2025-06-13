6b44ffae5069aac2fb49dd34e385e6c7f0131dc3
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2025-06-13 16:48:06 +0500

    master modified

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -209,13 +209,13 @@
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
             <img src="{{ asset('assets/img/profile-img.jpg') }}" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+            <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
           </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
-              <h6>Kevin Anderson</h6>
-              <span>Web Designer</span>
+                <h6>{{ Auth::user()->name }}</h6>
+                <span>{{ Auth::user()->role ?? 'Member' }}</span>
             </li>
             <li>
               <hr class="dropdown-divider">
@@ -252,10 +252,15 @@
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="#">
-                <i class="bi bi-box-arrow-right"></i>
-                <span>Sign Out</span>
-              </a>
+                <a class="dropdown-item d-flex align-items-center" href="{{ route('logout') }}"
+                  onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    <i class="bi bi-box-arrow-right"></i>
+                    <span>{{ __('Sign Out') }}</span>
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
             </li>
 
           </ul><!-- End Profile Dropdown Items -->
